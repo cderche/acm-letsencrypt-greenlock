a20d2d3637445223ded432fd5f2209b54238c766
Cyrille <c.derche@me.com>
2019-05-13 14:00:15 +0100

    Domain validation

--- src/app.js
+++ src/app.js
@@ -3,8 +3,10 @@ const app = express();
 
 app.get('/register', (req, res) => {
     if (!req.query.hasOwnProperty('hostname')) {
+        res.sendStatus(400);
+    } else if (!require('./domainValidator').isValid(req.query.hostname)) {
         res.sendStatus(422);
     }
-})
+});
 
 module.exports = app
--- src/app.test.js
+++ src/app.test.js
@@ -4,7 +4,12 @@ const app = require('./app');
 describe('GET /register', () => {
     test('No hostname was entered.', () => {
         return request(app).get("/register").then(response => {
-            expect(response.statusCode).toBe(422);
+            expect(response.statusCode).toBe(400);
         });
     });
+    test("Invalid hostname was entered", () => {
+        return request(app).get("/register?hostname=..com").then(response => {
+            expect(response.statusCode).toBe(422);
+        });
+    })
 });
